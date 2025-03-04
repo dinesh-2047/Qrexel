8e224f82f1eb115e34607ff669924def07d017c0
Rajdip-Pal <rajdippal2005@gmail.com>
2025-03-04 11:59:11 +0530

    Responsive footer is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,44 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Scanner from "./pages/Scanner";
-import Report from "./pages/Report";
-import QrCodes  from "./components/ui/QrCodes ";
-import Loader from "./pages/Loader";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Scanner from './pages/Scanner';
+import Report from './pages/Report';
+import QrCodes from './components/ui/QrCodes ';
+import Loader from './pages/Loader';
+import Footer from './components/Footer';
 
 function App() {
-  const [showimg ,setShowimg] = useState(true)
-      useEffect(()=>{
-          setTimeout(() => {
-              setShowimg(false);
-          }, 3000);
-      },[])
+  const [showimg, setShowimg] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowimg(false);
+    }, 3000);
+  }, []);
 
   return (
     <div>
-      {
-        showimg?
-        <Loader/>:
-    <BrowserRouter>
-      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 relative transition-all duration-300">
-        <Navbar />
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/scanner" element={<Scanner />} />
-            <Route path="/report" element={<Report />} />
-            <Route path="/qrcode" element={<QrCodes />} />
-          </Routes>
-        </main>
-        <Toaster position="bottom-right" />
-      </div>
-    </BrowserRouter>
-        }
+      {showimg ? (
+        <Loader />
+      ) : (
+        <BrowserRouter>
+          <div className='min-h-screen bg-gray-100 dark:bg-gray-900 relative transition-all duration-300'>
+            <Navbar />
+            <main className='container mx-auto px-4 py-8'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/scanner' element={<Scanner />} />
+                <Route path='/report' element={<Report />} />
+                <Route path='/qrcode' element={<QrCodes />} />
+              </Routes>
+            </main>
+            <Toaster position='bottom-right' />
+          </div>
+          <Footer />
+        </BrowserRouter>
+      )}
     </div>
-    
   );
 }
 
