21c2cadf37256437dc0d81728fa8173cb300190b
Rajdip-Pal <rajdippal2005@gmail.com>
2025-03-08 18:08:14 +0530

    Adding custom cursour with added assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode:["class", "class"],
+  darkMode: ['class', 'class'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+      cursor: {
+        custom: "url('./public/cursours/icons8-select-cursor-30.png'), auto",
+        pointer: "url('./public/cursours/icons8-hand-cursor-30.png'), auto",
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
